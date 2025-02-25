c626a24f1fcd24ebcc2ffaae1a2f72bd38433531
Rhamon Almeida <rhamonalmeida.dev@hotmail.com>
2025-02-25 19:54:36 -0300

    alteraçao no código

---
+++ src/services/email.service.ts
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+})
+
+const mailOptions = {
+    from: `"API TESTE" <${process.env.EMAIL_USER}>`,
+    to: "nicolas@email.com",
+    subject: "Enviando email teste",
+    text: "Aqui vai o conteúdo"
+}
+
+async function main() {
+    const envio = await transporter.sendMail(mailOptions)
+    console.log(envio);
+    
+}
+
+main().catch(console.error)
